5af03621eff0c303a6ee377e89baf34c1709407e
williammck <wjmck@me.com>
2014-09-08 07:38:28 -0500

    added option to disable debug shell

--- new/config.py
+++ new/config.py
@@ -1,3 +1,4 @@
 DEBUG=True
+DEBUG_SHELL=False
 HOST="167.88.116.28"
 SQLALCHEMY_DATABASE_URI = "mysql://nerd2:nerd2website@localhost/nerd2"
--- new/run.py
+++ new/run.py
@@ -10,7 +10,10 @@ if len(args) != 1:
 elif args[0] == 'www':
     from www.app import app
     app.config.from_object(config)
-    app.run(app.config.get("HOST", "0.0.0.0"), app.config.get("PORT", 5000))
+    app.run(
+        app.config.get("HOST", "0.0.0.0"),
+        app.config.get("PORT", 5000),
+        use_evalex=("DEBUG_SHELL", True))
 elif args[0] == 'scrape':
     from scrape.app import app
     app.config = config
