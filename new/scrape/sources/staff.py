4d73a7119284948ef6b9176676ba4486e2b6667c
Barney Gale <barney.gale@gmail.com>
2014-09-14 17:57:35 +0100

    Simplified staff page scraping

--- new/scrape/sources/staff.py
+++ new/scrape/sources/staff.py
@@ -1,8 +1,6 @@
 import json
-import urllib
-import math
+import re
 
-from bs4 import BeautifulSoup
 from twisted.internet import defer, task
 from twisted.web import client
 client.HTTPClientFactory.noisy = False
@@ -12,79 +10,43 @@ from sources import Source
 
 class StaffSource(Source):
 
-    def _query(self, **request_data):
-        d0 = defer.Deferred()
-
-        def _callback(d):
-            d0.callback(BeautifulSoup(d.decode('utf8')))
-
-        def _errback(e):
-            d0.errback(e)
-
-        url = 'http://nerd.nu/staff'
-
-        d1 = client.getPage(url)
-        d1.addCallbacks(_callback, _errback)
-
-        return d0
-
     def start(self):
         task.LoopingCall(self.update).start(60*60*2)
 
     def update(self):
-        d = self._query()
+        d = client.getPage('http://nerd.nu/staff')
         d.addCallback(self._handle_staff)
 
     def _handle_staff(self, response_data):
-        content = response_data
-
         staff = {}
 
-        for role in content.find_all('h2'):
-            name = role.string
+        # Strip linebreaks and excessive whitespace
+        response_data = re.sub('[\r\n\t ]+', ' ', response_data)
 
-            if name == 'Server Admins':
-                h3s = role.find_all_next('h3')
-                for h3 in h3s:
-                    name = h3.string+' Admins'
-                    ul = h3.find_next('ul', class_=["playerlist", "playerlist2"])
+        # Grab sections
+        # We actually grab the title of the next section, so we have to swap
+        #   at the end of each iteration
+        title = None
+        for section, level, new_title in re.findall('(.*?)<h([0-9]).*?>([^<]+)', response_data):
+            print level, new_title
+            if title:
+                # In Memoriam section returns players as a dictionary
+                if title == "In Memoriam":
+                    players = dict(re.findall('&p=(.*?)".*?clear: both">(.*?)\s*<', section))
 
-                    players = []
+                # Otherwise use a list
+                else:
+                    players = re.findall('&p=(.*?)"', section)
 
-                    for li in ul.find_all('li'):
-                        p = li.find('p')
-                        player = p.string
-                        players.append(player)
+                if players:
+                    staff[title] = players
 
-                    staff[name] = players
-
-            elif name == 'In Memoriam':
-                uls = role.find_all_next('ul', class_=["playerlist", "playerlist2"])
-
-                players = {}
-
-                for ul in uls:
-                    li = ul.find('li')
-                    p = li.find('p')
-                    player = p.string
-
-                    text = p.find_next('p').string
-
-                    players[player] = text
-
-                staff[name] = players
-
+            if level == "2":
+                title = new_title
+            elif level == "3":
+                title = "%s Admins" % new_title
             else:
-                ul = role.find_next('ul', class_=["playerlist", "playerlist2"])
-
-                players = []
-
-                for li in ul.find_all('li'):
-                    p = li.find('p')
-                    player = p.string
-                    players.append(player)
-
-                staff[name] = players
+                title = None
 
 
         staff = json.dumps(staff)
