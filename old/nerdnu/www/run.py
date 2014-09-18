6be51a7c34ed38081ac58c5e306aa79d8b3bad9c
williammck <git@williammck.net>
2014-09-18 08:25:11 -0500

    removed old folder

--- old/nerdnu/www/run.py
+++
@@ -1,6 +0,0 @@
-#from flup.server.fcgi import WSGIServer
-from nerdnu import app
-
-if __name__ == '__main__':
-    #WSGIServer(app, bindAddress='/tmp/nerd-fcgi.sock', umask=777).run()
-    app.run()
--- old/nerdnu/www/static/player.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    draw_model('model', 'scratch', $('#player-name').text(), 5, true);
-});
