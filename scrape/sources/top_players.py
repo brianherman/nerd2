ad1c164c55b658a24e4fe798581c8b98e0d824a1
williammck <git@williammck.net>
2014-09-24 16:58:17 -0500

    removed unnecessary code from top_players scraper

--- scrape/sources/top_players.py
+++ scrape/sources/top_players.py
@@ -37,31 +37,6 @@ class TopPlayersSource(Source):
             d = self._query(server)
             d.addCallback(self._handle_stats, server)
 
-    def _handle_data(self, data):
-        r = data['storage']['usagestats']
-
-        sp = json.loads(r)['players']
-        sl = {}
-        for k,v in sp.items():
-            sl[k] = v['min']
-        su = sorted(sl.items(), key=lambda x:x[1], reverse=True)
-
-        stats = []
-        count = 0
-        for stat in su:
-            if count == 10:
-                break
-            def pretty_time(time):
-                hours = time / 3600000.0
-                h = time / 3600000
-                m =  int((hours - h) * 60)
-                return str(h)+'h '+str(m)+'m'
-            time = pretty_time(stat[1])
-            stats.append({'username': stat[0], 'time': time})
-            count = count+1
-
-        return json.dumps(stats)
-
     def _handle_stats(self, response_data, server):
         raw_data = response_data['storage']['usagestats']
         data = json.loads(raw_data)['players']
