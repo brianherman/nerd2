98441ee468780c4f1721b3c74e77a25e073b278c
williammck <git@williammck.net>
2014-09-25 12:18:21 -0500

    added online players to frontend

--- www/blueprints/server/__init__.py
+++ www/blueprints/server/__init__.py
@@ -12,13 +12,21 @@ def _index(server):
     addr = {'creative': 'c.nerd.nu', 'survival': 's.nerd.nu', 'pve': 'p.nerd.nu'}
     current_rev = int(Cache.query.filter_by(key='CURRENT_REVISION_'+server.upper()).first().value)
 
+    if server in ['creative', 'pve']:
+        players = json.loads(Cache.query.filter_by(key='MC_%s_ONLINE' % server.upper()).first().value)
+        top_players = PlayerTime.query.filter_by(server=server).order_by(PlayerTime.seconds.desc()).limit(10).all()
+    else:
+        players = None
+        top_players = None
+
     options = {
         'title': server,
         'addr': addr,
+        'players': players,
         'subreddit': json.loads(Cache.query.filter_by(key='REDDIT_POSTS').first().value),
         #'github': json.loads(Cache.query.filter_by(key='GITHUB_'+server.upper()+'_ISSUES').first().value),
         'current_rev': current_rev,
-        'top_players': PlayerTime.query.filter_by(server=server).order_by(PlayerTime.seconds.desc()).limit(10).all()
+        'top_players': top_players
     }
 
     return render_template('server.html', **options)
