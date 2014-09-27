9c36b1bf5757cc2bc5b28b2a101aeb5571f44df5
Barney Gale <barney.gale@gmail.com>
2014-09-27 21:56:49 +0100

    Move api_call logic into scraper app (rather than each source). Also throttle HTTP requests.

--- scrape/app.py
+++ scrape/app.py
@@ -1,14 +1,52 @@
-from twisted.internet import reactor
+import urllib
+import requests
+
+from twisted.internet import reactor, task
+
+from twisted.web import client
+client.HTTPClientFactory.noisy = False
 
 import sources
 
 class ScrapeApp(object):
     config = None
 
+    def __init__(self):
+        self.api_calls = []
+        task.LoopingCall(self.tick).start(0.1)
+
     def run(self):
         for source in sources.sources:
-            s = source(self.config)
+            s = source()
+            s.config = self.config
+            s.api_call = self.api_call
             s.start()
         reactor.run()
 
+    def tick(self):
+        if len(self.api_calls) > 0:
+            method, payload = self.api_calls.pop(0)
+            #print "sending", method
+            #client.getPage(self.config.API_URL+method,
+            #               method='post',
+            #               postdata=urllib.urlencode(payload),
+            #               headers={'Content-Type':'application/x-www-form-urlencoded'})
+            requests.post(self.config.API_URL+method, data=payload)
+
+    def api_call(self, method, **kwargs):
+        payload = None
+        if method == "update_cache":
+            #print method, kwargs['key']
+            payload = {'key': kwargs['key'], 'value': kwargs['value']}
+        elif method == "update_creation":
+            #print method, kwargs['name']
+            payload = {'name': kwargs['name'], 'server': kwargs['server'],
+                'revision': kwargs['revision'], 'x': kwargs['x'], 'z': kwargs['z']}
+        elif method == "update_modreq":
+            payload = kwargs
+        elif method == "update_playertime":
+            payload = {'playername': kwargs['playername'], 'server': kwargs['server'], 'seconds': kwargs['seconds']}
+
+        if payload:
+            self.api_calls.append((method, payload))
 app = ScrapeApp()
--- scrape/sources/__init__.py
+++ scrape/sources/__init__.py
@@ -1,24 +1,5 @@
-import requests
-
-
 class Source(object):
-    config = object
-    def __init__(self, config):
-        self.config = config
-
-    def api_call(self, method, **kwargs):
-        if method == "update_cache":
-            payload = {'key': kwargs['key'], 'value': kwargs['value']}
-            requests.post(self.config.API_URL+'update_cache', data=payload)
-        if method == "update_creation":
-            payload = {'name': kwargs['name'], 'server': kwargs['server'],
-                'revision': kwargs['revision'], 'x': kwargs['x'], 'z': kwargs['z']}
-            requests.post(self.config.API_URL+'update_creation', data=payload)
-        if method == "update_modreq":
-            r = requests.post(self.config.API_URL+'update_modreq', data=kwargs)
-        if method == "update_playertime":
-            payload = {'playername': kwargs['playername'], 'server': kwargs['server'], 'seconds': kwargs['seconds']}
-            requests.post(self.config.API_URL+'update_playertime', data=payload)
+    pass
 
 
 import wiki, mumble, irc, mc_ping, top_players, reddit, forums, staff, modreq
