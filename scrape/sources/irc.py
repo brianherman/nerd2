ea13688d837474fc3b563cdcceb9fe53a9ffd493
William McKinnerney <git@williammck.net>
2015-03-06 20:05:18 -0600

    kinda crappy fix for irc - as channel is private now, you can use a bouncer to get around it

--- scrape/sources/irc.py
+++ scrape/sources/irc.py
@@ -1,16 +1,19 @@
 import re
 
-from twisted.internet import reactor, task
+from twisted.internet import reactor, task, ssl
 from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import LineReceiver
+
+from OpenSSL import SSL
 
 from sources import Source
 
 
 class IRCProtocol(LineReceiver):
     def connectionMade(self):
-        self.sendLine("NICK FooBot")
-        self.sendLine("USER bot bot bot :bot")
+        self.sendLine("NICK %s" % self.config.IRC_NICK)
+        self.sendLine("USER %s 0 * :%s" % (self.config.IRC_USER, self.config.IRC_REALNAME))
+        self.sendLine("PASS %s:%s" % (self.config.IRC_USER, self.config.IRC_PASSWORD))
 
     def connectionLost(self, reason=None):
         self.factory.stopFactory()
@@ -23,18 +26,37 @@ class IRCProtocol(LineReceiver):
 
         welcome = re.match(r'^:\S+ 002', line)
         if welcome:
-            self.sendLine("LIST #RedditMC")
+            self.sendLine("LIST %s" % self.config.IRC_CHANNEL)
             return
 
         list_ = re.match(r'^:\S+ 322 (\S+) (\S+) (\d+)', line)
         if list_:
             self.factory.got_users(int(list_.group(3)))
+            self.done = True
             self.sendLine("QUIT :quit")
             self.transport.loseConnection()
+
+        list_end = re.match(r'^:\S+ 323', line)
+        if list_end and not self.done:
+            print "Couldn't get IRC users. Is the channel private or secret?"
+            self.factory.got_users(0)
+            self.sendLine("QUIT :quit")
+            self.transport.loseConnection()
+
+    def __init__(self):
+        self.done = False
 
 
 class IRCFactory(ClientFactory):
     protocol = IRCProtocol
+
+
+class CtxFactory(ssl.ClientContextFactory):
+    def getContext(self):
+        self.method = SSL.SSLv23_METHOD
+        ctx = ssl.ClientContextFactory.getContext(self)
+
+        return ctx
 
 
 class IRCSource(Source):
@@ -44,7 +66,12 @@ class IRCSource(Source):
     def update(self):
         factory = IRCFactory()
         factory.got_users = self.got_users
-        reactor.connectTCP("irc.gamesurge.net", 6667, factory)
+        factory.protocol.config = self.config
+
+        if self.config.IRC_SSL:
+            reactor.connectSSL(self.config.IRC_SERVER, self.config.IRC_PORT, factory, CtxFactory())
+        else:
+            reactor.connectTCP(self.config.IRC_SERVER, self.config.IRC_PORT, factory)
 
     def got_users(self, count):
         self.api_call("update_cache",
